509af121c4a48c0f1427e4c837a18affcf974cc8
vincentherrmann <vincent.herrmann@web.de>
2019-01-06 15:55:34 +0100

    only try installation

--- setup_system.py
+++ setup_system.py
@@ -3,50 +3,46 @@ import sys
 
 
 def install(package):
-    if type(package) is str:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", package])
-    else:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "--user"] + package)
-    print("installed", package)
+    try:
+        if type(package) is str:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", package])
+        else:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "--user"] + package)
+        print("installed", package)
+    except:
+        print("error while installing", package)
 
 try:
     import torch
 except:
     install(['torch', 'torchvision'])
-    import torch
 
 try:
     import librosa as lr
 except:
     install('librosa')
-    import librosa as lr
 
 try:
     import matplotlib.pyplot as plt
 except:
     install("matplotlib")
-    import matplotlib.pyplot as plt
 
 try:
     from pydrive.auth import GoogleAuth
     from pydrive.drive import GoogleDrive
 except:
     install("PyDrive")
-    from pydrive.auth import GoogleAuth
-    from pydrive.drive import GoogleDrive
 
 try:
     import tensorboardX
 except:
     install("tensorboardX")
-    import tensorboardX
 
 try:
     import torchaudio
 except:
     subprocess.check_call(["sudo", "apt-get", "install", "sox", "libsox-dev", "libsox-fmt-all"])
     install("git+git://github.com/pytorch/audio")
-    import torchaudio
 
 print("all packages installed")
 
