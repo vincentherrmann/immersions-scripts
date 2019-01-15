fa5ee11adb44dec3ce02479fe84655051068c73c
vincentherrmann <vincent.herrmann@web.de>
2019-01-15 22:36:00 +0100

    move test task forward

--- cpc_training.py
+++ cpc_training.py
@@ -146,6 +146,11 @@ def main():
         snapshot_manager.upload_latest_files()
 
     def dummy_validation_function():
+        if not task_thread.isAlive():
+            print("start task test")
+            task_data, task_labels = trainer.calc_test_task_data(batch_size=args.batch_size, num_workers=4)
+            task_thread(target=task_function, args=(task_data, task_labels, trainer.training_step))
+
         losses, accuracies, mean_score = trainer.validate(batch_size=args.batch_size, num_workers=4, max_steps=300)
         print(losses, accuracies, mean_score)
         logger.writer.add_scalar("score mean", mean_score, trainer.training_step)
@@ -156,11 +161,6 @@ def main():
             logger.writer.add_scalar("validation accuracy/step " + str(step),
                                      accuracies[step].item(),
                                      trainer.training_step)
-
-        if not task_thread.isAlive():
-            print("start task test")
-            task_data, task_labels = trainer.calc_test_task_data(batch_size=args.batch_size, num_workers=4)
-            task_thread(target=task_function, args=(task_data, task_labels, trainer.training_step))
         return torch.mean(losses).item(), torch.mean(accuracies).item()
 
     def task_function(task_data, task_labels, step):
